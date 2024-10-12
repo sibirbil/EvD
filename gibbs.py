dc8d12ef15bc056212e398a9a6bf8b6304130239
Eren Mehmet KIRAL <erenmehmetkiral@emkmac.protonmail.com>
2024-10-12 22:46:32 +0900

    some aesthethic changes to MALA function

--- gibbs.py
+++ gibbs.py
@@ -40,15 +40,13 @@ F_grad = jax.grad(lambda x: jnp.reshape(F(x),()))
 hyps0 = (F, F_grad, 0.1)
 
 
-def acceptance_ratio(x, x_maybe, xi, hyps):
-    func, grad_func, eta = hyps
-    w = x - x_maybe + eta*grad_func(x_maybe)
-    v = (1/(4*eta))*jnp.inner(w,w)
-    u = 1/2* jnp.inner(xi,xi) - func(x_maybe) + func(x) - v
-    return jnp.reshape(jnp.exp(jnp.minimum(u,0)),())
-
 
 def MALA_step(state, hyps):
+    """
+    Computes the next step in Metropolis Adjusted Langevin Algorithm.
+    Which either accepts the Langevin step or stays at current point.
+    Either case also outputs a new pseudorandom number generator key.
+    """
     func, grad_func, eta = hyps
     x, key = state
     
@@ -57,8 +55,15 @@ def MALA_step(state, hyps):
     g = grad_func(x)
     x_maybe, xi = langevin_step(x, g, eta, key)
     
+    # inlaid function for convenience computes
+    def acceptance_ratio():
+        w = x - x_maybe + eta*grad_func(x_maybe)
+        v = (1/(4*eta))*jnp.inner(w,w)
+        u = 1/2* jnp.inner(xi,xi) - func(x_maybe) + func(x) - v
+        return jnp.reshape(jnp.exp(jnp.minimum(u,0)),())
+
     # Compute acceptance ratio
-    alpha = acceptance_ratio(x, x_maybe, xi, hyps)
+    alpha = acceptance_ratio()
     
     # Define acceptance and rejection steps
     def accept():
