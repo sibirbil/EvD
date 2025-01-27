2bbd6ae6f9fd71407f32b65501ffddc0058f0a80
Eren Mehmet KIRAL <erenmehmetkiral@emkmac.protonmail.com>
2025-01-27 23:02:34 +0900

    finished fico_mlp comparing risky vs sensitive

--- scripts/fico_mlp.py
+++ scripts/fico_mlp.py
@@ -5,7 +5,7 @@ import optax
 import pandas as pd
 from functools import partial
 import flax.linen as nn
-
+from xgboost import XGBClassifier
 
 import create_datasets
 import nets
@@ -53,11 +53,13 @@ N_noise = 1000
 noise_std = 0.01
 noised_params = vi_opt.gaussian_single_std_samples(noise_key, ts.params, noise_std, N_noise)
 
-
+## score of a single set of params on a given dataset
 def score(params, X, y):
     logits = mlp.apply(params, X, is_training = False)
     preds = jnp.argmax(logits, axis = 1)
     return (preds == y).mean()
+
+
 
 
 ## This defines the loss function on the parameters for using MALA.
@@ -116,7 +118,24 @@ def G_function_sensitive(
     
     return jax.jit(G)
 
-betaG = 1000.
+
+clsfr = XGBClassifier(
+    subsample=0.6, 
+    reg_lambda=5, 
+    reg_alpha=10, 
+    n_estimators=200, 
+    min_child_weight=1, 
+    max_depth=8, 
+    learning_rate=0.05, 
+    gamma=1, 
+    colsample_bytetree=0.5
+    )
+clsfr.fit(X_train, y_train)
+print(f"The xgboost classifier has score \
+      {clsfr.score(X_train, y_train)} on training set \
+      and {clsfr.score(X_test, y_test)} on test set.")
+
+betaG = 100.
 etaG = 0.01/betaG
 N_data = 20000
 
@@ -131,30 +150,56 @@ _, traj_x = langevin.MALA_chain(x_state, hypsG, N_data)
 # see their predictions under original parameters
 traj_x_thetastar_logits = mlp.apply(ts.params, traj_x, is_training = False)
 traj_x_thetastar_probs = jax.nn.softmax(traj_x_thetastar_logits)
-traj_x_thetastar_preds = jnp.argmax(traj_x_thetastar_probs, axis = -1)
+traj_x_thetastar_preds = jnp.argmax(traj_x_thetastar_probs, axis = -1)  # shape (N_data,)
 # see their predictions under noised parameters
 traj_x_noised_logits = jax.vmap(mlp.apply, in_axes = (0, None, None))(noised_params, traj_x, False)
 traj_x_noised_probs = jax.nn.softmax(traj_x_noised_logits)
-traj_x_noised_preds = jnp.argmax(traj_x_noised_logits, axis = -1)
+traj_x_noised_preds = jnp.argmax(traj_x_noised_logits, axis = -1)   # shape (N_noise, N_data)
+
+sameornot = (traj_x_noised_preds == traj_x_thetastar_preds) # shape (N_Noise, N_data)
+# persistence of predictions
+persistence = jnp.mean(sameornot, axis = 0)
+print(f"The percent of persistence {persistence[-100:]}")
+
+
 
 # compare the two predictions
 noised_scores = jax.vmap(score, in_axes = (0,None, None))(noised_params, traj_x, traj_x_thetastar_preds)
 
-inverted = scaler.inverse_transform(traj_x)
-generated_df = pd.DataFrame(jnp.column_stack([traj_x_thetastar_preds, inverted]), columns = df.columns)
+inverted_noised_traj = scaler.inverse_transform(traj_x)
+generated_noised_df = pd.DataFrame(jnp.column_stack([traj_x_thetastar_preds, inverted_noised_traj]), columns = df.columns)
 
 test_logits = mlp.apply(ts.params, X_test, is_training =False)
 test_probs = jax.nn.softmax(test_logits)
 test_preds = jnp.argmax(test_probs, axis = -1)
 
 # measuring how robust the test_set predictions are to adding of noise.
-test_scores = jax.vmap(score, in_axes = (0, None, None))(noised_params, X_test, test_preds)
+test_noised_logits = jax.vmap(mlp.apply, in_axes = (0, None, None))(noised_params, X_test, False)
+test_noised_preds = jnp.argmax(test_noised_logits, axis = -1)
+test_persistence = jnp.mean((test_noised_preds == test_preds), axis = 0)  #shape (N_test,)
 
 G_risky = G_function_risky(traj_params, jnp.array([1,1]), mlp, betaG)
 hypsG_risky = G_risky, jax.grad(G_risky), etaG
 _, risky_traj_x = langevin.MALA_chain(x_state, hypsG_risky, N_data)
 
+
 risky_traj_logits = mlp.apply(ts.params, risky_traj_x, is_training = False)
 risky_traj_probs = jax.nn.softmax(risky_traj_logits)
 risky_traj_preds = jnp.argmax(risky_traj_probs, axis = -1)
 
+inverted_risky_traj = scaler.inverse_transform(risky_traj_x)
+generated_risky_df = pd.DataFrame(jnp.column_stack([risky_traj_preds, inverted_risky_traj]), columns = df.columns)
+
+test_df = pd.DataFrame(jnp.column_stack([y_test, scaler.inverse_transform(X_test)]), columns = df.columns)
+test0_df = test_df[test_df['RiskPerformance']==0]
+test1_df = test_df[test_df['RiskPerformance']==1]
+
+noise_traj_prob_std = jnp.std(traj_x_thetastar_probs[-N_data//2:, 0])
+risky_traj_prob_std = jnp.std(risky_traj_probs[-N_data//2:, 0])
+print(f"the std of noise {noise_traj_prob_std} and risky samples are {risky_traj_prob_std}")
+
+import plotting
+
+figure = plotting.feature_comparison_histograms([generated_noised_df, generated_risky_df, test0_df, test1_df],
+                                                labels = ['generated_noised', 'generated_risky', 'test_with_label0', 'test_with_label1'])
+figure.show()
